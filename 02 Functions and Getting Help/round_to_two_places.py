9f95532c22d67abaffe1036a41f0ff17b1255518
Andrey Yohanes <andrey.yohanes@gmail.com>
2021-03-24 00:57:36 +0700

    Create round_to_two_places.py

---
+++ 02 Functions and Getting Help/round_to_two_places.py
@@ -0,0 +1,7 @@
+def round_to_two_places(num):
+    """Return the given number rounded to two decimal places. 
+    
+    >>> round_to_two_places(3.14159)
+    3.14
+    """
+    return round(num, 2)
