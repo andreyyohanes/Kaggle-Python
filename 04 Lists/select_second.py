c1e36bc8a13ba3392c0d6db2c25117b1219f131c
Andrey Yohanes <andrey.yohanes@gmail.com>
2021-04-05 21:39:36 +0700

    Create select_second.py

---
+++ 04 Lists/select_second.py
@@ -0,0 +1,7 @@
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    if len(L) < 2:
+        return None
+    return L[1]
