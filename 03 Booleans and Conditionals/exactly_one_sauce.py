dc1d216de3ec4274de3ca970e5d956cc3799fbce
Andrey Yohanes <andrey.yohanes@gmail.com>
2022-03-22 00:30:00 +0700

    Delete exactly_one_sauce.py

--- 03 Booleans and Conditionals/exactly_one_sauce.py
+++
@@ -1,5 +0,0 @@
-def exactly_one_sauce(ketchup, mustard, onion):
-    """Return whether the customer wants either ketchup or mustard, but not both.
-    (You may be familiar with this operation under the name "exclusive or")
-    """
-    return (ketchup and not mustard) or (mustard and not ketchup)
