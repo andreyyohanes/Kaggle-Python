407238132c33e2fa4e9661de98368b242447e654
Andrey Yohanes <andrey.yohanes@gmail.com>
2022-03-22 00:39:42 +0700

    Delete exactly_one_topping.py

--- 03 Booleans and Conditionals/exactly_one_topping.py
+++
@@ -1,5 +0,0 @@
-def exactly_one_topping(ketchup, mustard, onion):
-    """Return whether the customer wants exactly one of the three available toppings
-    on their hot dog.
-    """
-    return (int(ketchup) + int(mustard) + int(onion)) == 1
