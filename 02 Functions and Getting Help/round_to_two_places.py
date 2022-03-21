c29933c8a36ae1e5e81d60c73f15a39868434e53
Andrey Yohanes <andrey.yohanes@gmail.com>
2022-03-21 21:56:08 +0700

    Delete round_to_two_places.py

--- 02 Functions and Getting Help/round_to_two_places.py
+++
@@ -1,7 +0,0 @@
-def round_to_two_places(num):
-    """Return the given number rounded to two decimal places. 
-    
-    >>> round_to_two_places(3.14159)
-    3.14
-    """
-    return round(num, 2)
