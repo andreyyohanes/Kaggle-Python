fb987061397b278ee82e9f6a42e30f85847a76e5
Andrey Yohanes <andrey.yohanes@gmail.com>
2022-03-22 16:53:31 +0700

    Delete fashionably_late.py

--- 04 Lists/fashionably_late.py
+++
@@ -1,5 +0,0 @@
-def fashionably_late(arrivals, name):
-    """Given an ordered list of arrivals to the party and a name, return whether the guest with that
-    name was fashionably late.
-    """
-    return len(arrivals) / 2 <= arrivals.index(name) < len(arrivals) - 1
